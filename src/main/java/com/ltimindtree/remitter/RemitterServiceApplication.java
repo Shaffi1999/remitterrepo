b2be3c0be6888bf79479e42576d06ed146a04caa
18 <18@host.docker.internal>
2023-01-04 22:47:13 -0800

    Created / and greeting with the name

---
+++ src/main/java/com/ltimindtree/remitter/RemitterServiceApplication.java
@@ -0,0 +1,29 @@
+package com.ltimindtree.remitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RemitterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemitterServiceApplication.class, args);
+	}
+	@GetMapping("/")
+	public String welcome()
+	{
+		return "remiiter Service Welcomes You!";
+	}
+	
+	@GetMapping("/{name}")
+	public String welcome1(@PathVariable String name)
+	{
+		return "remiiter Service Welcomes You"+name;
+	}
+	
+
+}
---
+++ src/test/java/com/ltimindtree/remitter/RemitterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltimindtree.remitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemitterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
